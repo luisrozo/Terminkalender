c5da92d928eb8ce34672ae453d9ea6e0ef8ae8d1
Luis Rozo <luis.rozobueno@alum.uca.es>
2017-04-27 11:51:44 +0200

    Update ServerDirection.java

--- core/src/com/project/terminkalender/websockets/ServerDirection.java
+++ core/src/com/project/terminkalender/websockets/ServerDirection.java
@@ -2,7 +2,7 @@ package com.project.terminkalender.websockets;
 
 public final class ServerDirection {
 	public static final int PORT = 8080;
-	public static final String IP = "localhost";
+	public static final String IP = "178.62.62.107";
 	public static final String serverDirection = "ws://"+ IP +":"+ PORT;
 	
 	@Override
